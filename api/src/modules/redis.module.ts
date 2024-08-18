86dbb1dc20c0b8e3442acc9b5b06cc39e6eebc56
sabrynico21 <sabry.nico21@gmail.com>
2024-08-18 18:02:40 +0200

    deleted references to namespaces

--- api/src/modules/redis.module.ts
+++ api/src/modules/redis.module.ts
@@ -12,7 +12,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         name: 'REDIS_CLIENT',
         transport: Transport.REDIS,
         options: {
-          host: 'redis.database.svc.cluster.local',
+          host: 'redis.default.svc.cluster.local',
           port: 6379,
         },
       },
